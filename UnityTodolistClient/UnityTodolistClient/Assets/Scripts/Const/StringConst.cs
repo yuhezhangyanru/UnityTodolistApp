2ccde28ada674dc59ed5112fe3ec9ef6d6cf2410
yuhezhangyanu <yuhezhangyanru@sina.com>
2018-05-09 21:32:01 +0800

    add const

---
+++ UnityTodolistClient/UnityTodolistClient/Assets/Scripts/Const/StringConst.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class StringConst
+{
+    /// <summary>
+    /// 本地配置对象名称
+    /// </summary>
+    public const string USER_CONFIG_NAME = "UserConfigObj";
+
+    /// <summary>
+    /// 记录prefab关联加载的对象
+    /// </summary>
+    public const string RESOURCE_OBJ_NAME = "ResourceMapObj";
+}
