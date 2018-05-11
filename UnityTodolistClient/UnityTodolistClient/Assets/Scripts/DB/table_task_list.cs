8dad63ac59948e5365f82e17019e12933f88cabd
yuhezhangyanu <yuhezhangyanru@sina.com>
2018-05-11 08:43:27 +0800

    rm useless db

---
+++ UnityTodolistClient/UnityTodolistClient/Assets/Scripts/DB/table_task_list.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System;
+[Serializable]
+public class table_task_list : table_data_base
+{
+        public table_task_list()
+            : base()
+        {
+        }
+
+		/*任务ID唯一，*/
+        public string id
+		{
+			set{ _id=value;}
+			get{return _id;}
+		}
+		/*任务所属用户ID*/
+        public string userid
+		{
+			set{ _userid=value;}
+			get{return _userid;}
+		}
+		/*任务内容描述*/
+        public string description
+		{
+			set{ _description=value;}
+			get{return _description;}
+		}
+		/*创建时间*/
+        public string createtime
+		{
+			set{ _createtime=value;}
+			get{return _createtime;}
+		}
+		/*要求完成的到期时间*/
+        public string endtime
+		{
+			set{ _endtime=value;}
+			get{return _endtime;}
+		}
+		/*实际完成时间*/
+        public string actualFinishTime
+		{
+			set{ _actualFinishTime=value;}
+			get{return _actualFinishTime;}
+		}
+
+		private string _id;
+		private string _userid;
+		private string _description;
+		private string _createtime;
+		private string _endtime;
+		private string _actualFinishTime;
+}
---
+++ UnityTodolistClient/UnityTodolistClient/Assets/Scripts/DB/user.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System;
+[Serializable]
+public class user : table_data_base
+{
+        public user()
+            : base()
+        {
+        }
+
+		/*用户ID*/
+        public string id
+		{
+			set{ _id=value;}
+			get{return _id;}
+		}
+		/*用户昵称*/
+        public string username
+		{
+			set{ _username=value;}
+			get{return _username;}
+		}
+		/*电子邮箱*/
+        public string email
+		{
+			set{ _email=value;}
+			get{return _email;}
+		}
+		/*电话号码*/
+        public string phonenumber
+		{
+			set{ _phonenumber=value;}
+			get{return _phonenumber;}
+		}
+		/*用户创建时间*/
+        public string createtime
+		{
+			set{ _createtime=value;}
+			get{return _createtime;}
+		}
+		/*上次登录时间*/
+        public string lastlogintime
+		{
+			set{ _lastlogintime=value;}
+			get{return _lastlogintime;}
+		}
+
+		private string _id;
+		private string _username;
+		private string _email;
+		private string _phonenumber;
+		private string _createtime;
+		private string _lastlogintime;
+}
